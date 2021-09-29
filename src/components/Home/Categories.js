8aecb812aded87351971d874c89ff03eaca15cda
Aarushi Shanker <aarushishanker3108@gmail.com>
2021-09-29 14:42:06 +0530

    style

--- src/components/Home/Categories.js
+++ src/components/Home/Categories.js
@@ -6,7 +6,7 @@ import {Container} from 'react-bootstrap'
 function Categories() {
     return (
         // <div style={{textAlign:"center",width:"100vw",marginTop:"150px",display:"flex",flexDirection:"column",justifyContent:"center",alignItems:"center",flexWrap:'wrap'}}>
-        <Container style={{textAlign:"center",width:"100vw",marginTop:"150px"}}>
+        <Container style={{textAlign:"center",width:"100vw",marginTop:"90px"}}>
             <h2 className="heading" style={{fontSize:"30px"}}><b>HOW IT WORKS</b></h2>
             <p className="subheading" style={{fontSize:"18px"}}>Sea Basket delivers fresh sourced seafood in a few easy clicks</p>
             <h3 className="subHeading2" style={{fontSize:"20px",fontWeight:'600'}}>CATEGORIES</h3>
