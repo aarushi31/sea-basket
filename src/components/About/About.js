39a91ed0b70a9d762f8ca6561152ae9aa1a369a6
Aarushi Shanker <aarushishanker3108@gmail.com>
2021-10-04 12:12:37 +0530

    About us page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,29 +1,31 @@
 import React from 'react'
 import { Container,Row,Col } from 'react-bootstrap'
 import './About.css'
-import team from '../../images/team.jpg'
-import team2 from '../../images/team2.jpg'
+import brand from '../../images/brand 1.png'
+import team from '../../images/team 1.png'
 import Footer from '../Home/Footer/Footer'
 import i1 from '../../images/2672335.jpg'
 import i2 from '../../images/3343892.jpg'
 import i3 from '../../images/5127314.jpg'
 import i4 from '../../images/20943964.jpg'
-import hands from '../../images/hands.jpg'
-import founder from '../../images/founder.jpg'
+import mission from '../../images/mission.png'
+import founder from '../../images/founder 1.png'
 
 function About() {
     return (
         <>
         <Container>
-            <Row className="row">
-                <Col><img src={team} className="team-img"/></Col>
-                <Col className="text">
-                    We are SEA BASKET, we deep root in food craftsmanship, a passion built into a Company.
-                    SEABASKET’s goal is to build a Brand that is different in every way</Col>
+            <h2 className="about-heading">About us</h2>
+            <hr className="heading-hr"/>
+            <Row className="row" style={{marginTop:'100px'}}>
+                <Col><img src={brand} className="team-img"/></Col>
+                <Col className="text" style={{textAlign:'right'}}>
+                    <p>We are SEA BASKET, we deep root in food craftsmanship, a passion built into a Company.
+                    SEABASKET’s goal is to build a <b>Brand that is different in every way</b></p></Col>
             </Row>
             <Row className="row">
-                <Col className="text">Our passion is sourcing; our expertise is in processing. From our founding, we have focused on building a reputation for quality, integrity and reliability.</Col>
-                <Col><img src={team2} className="team-img"/></Col>
+                <Col className="text"><p>Our passion is sourcing; our expertise is in processing. From our founding, we have focused on <b>building a reputation for quality, integrity and reliability.</b></p></Col>
+                <Col><img src={team} className="team-img" style={{marginLeft:'10vw'}}/></Col>
             </Row>
             <div className="small-text">
                 <p>We know that craft matters. </p>
@@ -51,14 +53,14 @@ function About() {
             </div>
             <div className="lineContainer">
                 <div className="verticalLine"></div>
+                <span className="vertical-text">We believe</span>
             </div>
             <div className="small-text" style={{marginTop:'50px'}}>
-                <h2><b>We've learned</b></h2>
                 <center><p>That when you connect good people with good food, great things happen</p>
                 <p>We seek out fishing families that span generations, with a deep-rooted passion for their craft and a proven history of caring for their livestock and their environment.</p></center>
             </div>
             <div className="mission">
-                <img src={hands} style={{width:'40vw'}}/>
+                <img src={mission} style={{width:'40vw'}}/>
                 <div className="mission-text">
                     <h2>Our mission</h2>
                     <p>WE STRIVE to provide superb customer service, by utilizing a knowledgeable and courteous staff."</p>
@@ -71,7 +73,7 @@ function About() {
                 <h2>Meet our founders</h2>
                 <div className="founder">
                     <img src={founder} style={{width:'40vw'}}/>
-                    <p>Way back when a work trip took me to Mangalore Port, which put a spell on me; the fish there tasted so good and fresh, and so I began to ship them to Bengaluru for my home and, later, to my friends here in Bengaluru. Regularly for a long while friends would approach me asking for seafood from different states and every week friends of friends would get involved until the capacity of our living room wasn't up to the demand. I began to look for a solution and it was from this the idea of my own e-shop D2C first came up.<br/><br/>
+                    <p style={{textAlign:'right'}}>Way back when a work trip took me to Mangalore Port, which put a spell on me; the fish there tasted so good and fresh, and so I began to ship them to Bengaluru for my home and, later, to my friends here in Bengaluru. Regularly for a long while friends would approach me asking for seafood from different states and every week friends of friends would get involved until the capacity of our living room wasn't up to the demand. I began to look for a solution and it was from this the idea of my own e-shop D2C first came up.<br/><br/>
                     <em>Shaik Imran Pasha.A</em></p>
                 </div>
                 <div className="founder">
